a734ca9560c0d690084d2811b90d0ca03515f0e5
iFuSiiOnzZ <andreisystem@gmail.com>
2016-11-09 18:54:12 +0100

    Thread safe, fix identifier not found for debug_free in release

--- src/src/debug_memory.cpp
+++ src/src/debug_memory.cpp
@@ -15,75 +15,58 @@ typedef struct debug_memory_record
     struct debug_memory_record *Prev;
 } debug_memory_record;
 
-static debug_memory_record *LinkedListHead = NULL;
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-#include <windows.h>
-#include <Dbghelp.h>
-#pragma comment(lib, "Dbghelp.lib")
-
-static void SetCallStack(debug_memory_record *DebugRecord)
+typedef struct mutex_ticket
 {
-    void *callers[48] = { 0 };
-    SymInitialize(GetCurrentProcess(), NULL, TRUE);
+    unsigned long long volatile Ticket;
+    unsigned long long volatile Serving;
+} mutex_ticket;
 
-    int nCaptured = CaptureStackBackTrace(2, 48, callers, NULL);
-    DebugRecord->CallStackSize = nCaptured;
+///// Global variables
+static debug_memory_record *GlobalLinkedListHead = NULL;
+static mutex_ticket GlobalMutex = { 0 };
 
-    IMAGEHLP_LINE64 SimbolLine = { 0 };
-    SymSetOptions(SYMOPT_LOAD_LINES);
+///// Platform specific functions
+inline unsigned long long AtomicAddU64(unsigned long long volatile *Value, unsigned long long Addend);
+static void SetCallStack(debug_memory_record *DebugRecord);
 
-    for(int i = 0; i < nCaptured; i++)
-    {
-        char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME] = {  0 };
-        SYMBOL_INFO *Symbol = (PSYMBOL_INFO)buffer;
+inline void BeginMutex(mutex_ticket *Mutex);
+inline void EndMutex(mutex_ticket *Mutex);
 
-        Symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-        Symbol->MaxNameLen = MAX_SYM_NAME;
-
-        SymFromAddr(GetCurrentProcess(), (DWORD64) callers[i], 0, Symbol);
-        SimbolLine.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
-
-        unsigned long Displacement = 0;
-        SymGetLineFromAddr64(GetCurrentProcess(), (DWORD64) callers[i], &Displacement, &SimbolLine);
-
-        sprintf_s(DebugRecord->CallStack[i], 1024, "%24s (%64s > % 5d)", Symbol->Name, SimbolLine.FileName, SimbolLine.LineNumber);
-    }
-
-    SymCleanup(GetCurrentProcess());
-}
-///////////////////////////////////////////////////////////////////////////////////////////////
-
+///// General functions
 static void bzero(void *s1, size_t n)
 {
     char *t = (char *)s1;
     while (n-- != 0) *t++ = 0;
-    
 }
 
 void *debug_malloc_imp(size_t size, char *FileName, int FileLine)
 {
+    void *Memory = malloc(size);
+    if (!Memory) return Memory;
+
     debug_memory_record *NewRecord = (debug_memory_record *) malloc(sizeof(debug_memory_record));
     bzero(NewRecord, sizeof(debug_memory_record));
+
+    NewRecord->Memory = Memory;
     SetCallStack(NewRecord);
 
     NewRecord->FileName = FileName;
     NewRecord->FileLine = FileLine;
 
     NewRecord->MemorySize = size;
-    NewRecord->Prev = LinkedListHead;
+    NewRecord->Prev = GlobalLinkedListHead;
 
-    NewRecord->Memory = malloc(size);
-    LinkedListHead = NewRecord;
+    BeginMutex(&GlobalMutex);
+        GlobalLinkedListHead = NewRecord;
+    EndMutex(&GlobalMutex);
 
-    return NewRecord->Memory;
+    return Memory;
 }
 
 void debug_free_imp(void *Mem)
 {
-    debug_memory_record **SearchNode = &LinkedListHead;
     free(Mem);
+    debug_memory_record **SearchNode = &GlobalLinkedListHead;
 
     while(*SearchNode != NULL && (*SearchNode)->Memory != Mem)
     {
@@ -95,15 +78,17 @@ void debug_free_imp(void *Mem)
         return;
     }
 
-    debug_memory_record *DeleteNode = *SearchNode;
-    *SearchNode = DeleteNode->Prev;
+    BeginMutex(&GlobalMutex);
+        debug_memory_record *DeleteNode = *SearchNode;
+        *SearchNode = DeleteNode->Prev;
+    EndMutex(&GlobalMutex);
 
     free(DeleteNode);
 }
 
 void debug_print_imp(void)
 {
-    debug_memory_record *Node = LinkedListHead;
+    debug_memory_record *Node = GlobalLinkedListHead;
     size_t TotalBytes = 0, RemainingAllocations = 0;
 
     printf("======================== MEMORY ALLOCATIONS ========================\n");
@@ -111,7 +96,7 @@ void debug_print_imp(void)
     while(Node != NULL)
     {
         printf("  >  %s (Line % 5zd)    % 5zd bytes\n", Node->FileName, Node->FileLine, Node->MemorySize);
-        for(int i = 0; i < Node->CallStackSize; ++i) printf("          % 2d %s\n", i, Node->CallStack[i]);
+        for(unsigned int i = 0; i < Node->CallStackSize; ++i) printf("          % 2d %s\n", i, Node->CallStack[i]);
         if(Node->CallStackSize) printf("\n");
 
         TotalBytes += Node->MemorySize; 
@@ -124,3 +109,93 @@ void debug_print_imp(void)
     printf("====================================================================\n");
     printf("  >>>  %zd bytes in %zd allocations\n", TotalBytes, RemainingAllocations);
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef WIN64
+    #include <windows.h>
+    #include <immintrin.h>
+
+    #include <Dbghelp.h>
+    #pragma comment(lib, "Dbghelp.lib")
+
+    static void SetCallStack(debug_memory_record *DebugRecord)
+    {
+        void *callers[48] = { 0 };
+        SymInitialize(GetCurrentProcess(), NULL, TRUE);
+
+        int nCaptured = CaptureStackBackTrace(2, 48, callers, NULL);
+        DebugRecord->CallStackSize = nCaptured;
+
+        IMAGEHLP_LINE64 SimbolLine = { 0 };
+        SymSetOptions(SYMOPT_LOAD_LINES);
+
+        for (int i = 0; i < nCaptured; i++)
+        {
+            char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME] = { 0 };
+            SYMBOL_INFO *Symbol = (PSYMBOL_INFO)buffer;
+
+            Symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+            Symbol->MaxNameLen = MAX_SYM_NAME;
+
+            SymFromAddr(GetCurrentProcess(), (DWORD64)callers[i], 0, Symbol);
+            SimbolLine.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+
+            unsigned long Displacement = 0;
+            SymGetLineFromAddr64(GetCurrentProcess(), (DWORD64)callers[i], &Displacement, &SimbolLine);
+
+            sprintf_s(DebugRecord->CallStack[i], 1024, "%24s (%64s > % 5d)", Symbol->Name, SimbolLine.FileName, SimbolLine.LineNumber);
+        }
+
+        SymCleanup(GetCurrentProcess());
+    }
+
+    inline unsigned long long AtomicAddU64(unsigned long long volatile *Value, unsigned long long Addend)
+    {
+        return _InterlockedExchangeAdd64((__int64 volatile *)Value, Addend);
+    }
+
+    inline void BeginMutex(mutex_ticket *Mutex)
+    {
+        unsigned long long Ticket = AtomicAddU64(&Mutex->Ticket, 1);
+        while (Ticket != Mutex->Serving) { _mm_pause(); }
+    }
+
+    inline void EndMutex(mutex_ticket *Mutex)
+    {
+        AtomicAddU64(&Mutex->Serving, 1);
+    }
+#else
+    #ifndef UNUSED
+        #define UNUSED(x) (void)x
+    #endif
+
+    #ifndef STRINGIFY
+        #define STRINGIFY_(x) #x
+        #define STRINGIFY(x) STRINGIFY_(x)
+    #endif
+
+    #pragma message (__FILE__ "(" STRINGIFY(__LINE__) "): warning: No multithreading and callstack support")
+
+    static void SetCallStack(debug_memory_record *DebugRecord)
+    {
+        UNUSED(DebugRecord);
+    }
+
+    inline unsigned long long AtomicAddU64(unsigned long long volatile *Value, unsigned long long Addend)
+    {
+        UNUSED(Value); UNUSED(Addend);
+    }
+
+    inline void BeginMutex(mutex_ticket *Mutex)
+    {
+        UNUSED(Mutex);
+    }
+
+    inline void EndMutex(mutex_ticket *Mutex)
+    {
+        UNUSED(Mutex);
+    }
+#endif
+///////////////////////////////////////////////////////////////////////////////////////////////
